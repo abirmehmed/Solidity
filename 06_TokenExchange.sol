ad22922a193ac620ad2ad4479ec6b4856b0a9042
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-04-29 20:24:25 +0600

    Update 06_TokenExchange.sol

--- 06_TokenExchange.sol
+++ 06_TokenExchange.sol
@@ -1,19 +1,21 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 contract TokenExchange {
     address public token1;
     address public token2;
     uint public rate;
 
-    constructor(address _token1, address _token2, uint _rate) public {
+    constructor(address _token1, address _token2, uint _rate) {
         token1 = _token1;
         token2 = _token2;
         rate = _rate;
     }
 
     function exchange(uint amount) public {
-        require(Token(token1).transferFrom(msg.sender, address(this), amount));
-        require(Token(token2).transfer(msg.sender, amount * rate));
+        require(Token(token1).transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(Token(token2).transfer(msg.sender, amount * rate), "Transfer failed");
     }
 }
 
