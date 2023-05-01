420dbc6063b007d28e4cfe862daf12997960c0ac
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-05-01 12:45:21 +0600

    Update lottery.sol

--- 04_lottery/lottery.sol
+++ 04_lottery/lottery.sol
@@ -1,33 +1,41 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 contract Lottery {
-    address payable[] public players;
+    address payable[] players;
     uint public maxPlayers;
     uint public lotteryEndTime;
     bool public lotteryEnded;
 
-    constructor(uint _maxPlayers, uint _lotteryDuration) public {
+    constructor(uint _maxPlayers, uint _lotteryDuration) {
         maxPlayers = _maxPlayers;
-        lotteryEndTime = now + _lotteryDuration;
+        lotteryEndTime = block.timestamp + _lotteryDuration;
         lotteryEnded = false;
     }
 
     function enter() public payable {
         require(!lotteryEnded, "Lottery has already ended.");
-        require(now <= lotteryEndTime, "Lottery has expired.");
+        require(block.timestamp <= lotteryEndTime, "Lottery has expired.");
         require(players.length < maxPlayers, "Maximum number of players reached.");
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
 
     function pickWinner() public {
         require(!lotteryEnded, "Lottery has already ended.");
-        require(now > lotteryEndTime || players.length == maxPlayers, "Lottery is still ongoing.");
+        require(block.timestamp > lotteryEndTime || players.length == maxPlayers, "Lottery is still ongoing.");
         uint index = random() % players.length;
         players[index].transfer(address(this).balance);
         lotteryEnded = true;
     }
 
+    function reset() public {
+        require(lotteryEnded, "Lottery has not ended.");
+        lotteryEnded = false;
+        delete players;
+    }
+
     function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
     }
 }
